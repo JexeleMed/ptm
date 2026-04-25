2996d1e206848b6bc3d733b28f9c6224e270e851
Batirro <batirro34@protonmail.com>
2026-04-25 21:49:19 +0200

    fix: resolve A10/A22/A51 assembler errors in zad2/zad3

--- lab4/zad2.asm
+++ lab4/zad2.asm
@@ -1,43 +1,41 @@
 $NOMOD51
 $INCLUDE (reg517.inc)
 
-; --- DEFINICJE PORTÓW I ZMIENNYCH ---
-P5 equ 0F8H
-P7 equ 0DBH
-    
-LCDstatus  equ 0FF2EH       ; adres do odczytu gotowosci LCD (XDATA)
-LCDcontrol equ 0FF2CH       ; adres do podania bajtu sterujacego LCD (XDATA)
-LCDdataWR  equ 0FF2DH       ; adres do podania kodu ASCII na LCD (XDATA)
+LCDstatus  equ 0FF2EH
+LCDcontrol equ 0FF2CH
+LCDdataWR  equ 0FF2DH
 
-#define  HOME     0x80      ; kursor do 1 linii  
-#define  INITDISP 0x38      ; LCD init (8-bit mode)  
-#define  HOM2     0xC0      ; kursor do 2 linii  
-#define  LCDON    0x0E      ; LCD wlaczone, kursor wylaczony
-#define  CLEAR    0x01      ; czyszczenie ekranu (i powrot do 1 linii)
+MAX_CHARS  equ 32
+LINE2_POS  equ 16
+
+#define  INITDISP 0x38
+#define  HOM2     0xC0
+#define  LCDON    0x0E
+#define  CLEAR    0x01
 
     ORG 0000H
     LJMP start
 
     ORG 0100H
-        
-; --- MAKRA BEZPIECZNE ---
+
+; --- MAKRA ---
 
 LCDcntrlWR MACRO x
       LOCAL loop
-      PUSH DPH              ; Zabezpiecz wskaźnik danych
+      PUSH DPH
       PUSH DPL
-      PUSH ACC              ; Zabezpiecz zawartosc akumulatora
+      PUSH ACC
 loop: MOV  DPTR,#LCDstatus
       MOVX A,@DPTR
-      JB   ACC.7,loop       ; Czekaj na flage zajetosci (Busy Flag = 0)
+      JB   ACC.7,loop
       MOV  DPTR,#LCDcontrol
       MOV  A, x
-      MOVX @DPTR,A          ; Wyslij komende
+      MOVX @DPTR,A
       POP  ACC
       POP  DPL
       POP  DPH
       ENDM
-      
+
 LCDcharWR MACRO
       LOCAL tutu
       PUSH DPH
@@ -45,21 +43,21 @@ LCDcharWR MACRO
       PUSH ACC
 tutu: MOV  DPTR,#LCDstatus
       MOVX A,@DPTR
-      JB   ACC.7,tutu       ; Czekaj na flage zajetosci
+      JB   ACC.7,tutu
       MOV  DPTR,#LCDdataWR
-      POP  ACC              ; Przywroc kod ASCII
-      MOVX @DPTR,A          ; Wyslij znak
+      POP  ACC
+      MOVX @DPTR,A
       POP  DPL
       POP  DPH
       ENDM
-      
+
 init_LCD MACRO
       LCDcntrlWR #INITDISP
       LCDcntrlWR #CLEAR
       LCDcntrlWR #LCDON
       ENDM
 
-; --- PROCEDURY POMOCNICZE ---
+; --- PROCEDURY ---
 
 delay:  MOV R0, #15H
 one:    MOV R1, #0FFH
@@ -68,39 +66,39 @@ trzy:   DJNZ R2, trzy
         DJNZ R1, dwa
         DJNZ R0, one
         RET
-            
-putcharLCD: 
+
+putcharLCD:
         LCDcharWR
         RET
 
 ; --- PROGRAM GŁÓWNY ---
 
-start:  
+start:
     init_LCD
-    MOV R5, #0              ; R5 = Licznik znaków na ekranie (od 0 do 31)
+    MOV R5, #0
 
 scan_matrix:
-    MOV P5, #0EFH           ; Skanowanie linii 4
+    MOV P5, #0EFH
     MOV A, P7
     ANL A, #0FH
     CJNE A, #0FH, key_L4
 
-    MOV P5, #0DFH           ; Skanowanie linii 3
+    MOV P5, #0DFH
     MOV A, P7
     ANL A, #0FH
     CJNE A, #0FH, key_L3
 
-    MOV P5, #0BFH           ; Skanowanie linii 2
+    MOV P5, #0BFH
     MOV A, P7
     ANL A, #0FH
     CJNE A, #0FH, key_L2
 
-    MOV P5, #07FH           ; Skanowanie linii 1
+    MOV P5, #07FH
     MOV A, P7
     ANL A, #0FH
     CJNE A, #0FH, key_L1
 
-    SJMP scan_matrix        ; Powtarzaj, dopóki nic nie wciśnięto
+    SJMP scan_matrix
 
 key_L4: ORL A, #0EFH
         SJMP process_key
@@ -109,71 +107,62 @@ key_L3: ORL A, #0DFH
 key_L2: ORL A, #0BFH
         SJMP process_key
 key_L1: ORL A, #07FH
-        SJMP process_key
 
 process_key:
-    MOV R4, A               ; Zapisz zrekonstruowany kod w R4
+    MOV R4, A
 
 wait_for_release:
-    MOV P5, #00H            ; Ustaw 0 na wszystkich liniach
+    MOV P5, #00H
     MOV A, P7
     ANL A, #0FH
-    CJNE A, #0FH, wait_for_release ; Czekaj na puszczenie przycisku
-    ACALL delay             ; Opóźnienie na drgania styków (debounce)
+    CJNE A, #0FH, wait_for_release
+    ACALL delay
 
-    MOV A, R4               ; Odzyskaj kod z R4
-    MOV R2, #0              ; Indeks tabeli w R2
-    MOV DPTR, #SCAN_CODES   ; Szukamy kodu klawisza w tabeli
+    MOV R2, #0
+    MOV DPTR, #SCAN_CODES
 
-find_loop:
+search_code:
     MOV A, R2
-    MOVC A, @A+DPTR
-    JZ scan_matrix          ; Koda 00H to koniec tabeli, ignorujemy błąd
-    CJNE A, R4, next_code   ; Brak dopasowania -> szukaj dalej
-    SJMP manage_cursor      ; Znalazl kod! Idz do logiki ekranu
-
-next_code:
-    INC R2
-    SJMP find_loop
+    MOVC A, @A+DPTR         ; pobierz bajt z tabeli
+    JZ   scan_matrix        ; 00H = koniec tabeli, nieznany klawisz
+    XRL  A, R4              ; porównaj z zapisanym kodem (fix A22!)
+    JZ   manage_cursor      ; A==R4 gdy XRL daje 0
+    INC  R2
+    SJMP search_code        ; pętla blisko - SJMP wystarczy
 
 manage_cursor:
-    ; Sprawdzamy stan licznika w R5, aby wiedzieć gdzie wpisać znak
     MOV A, R5
-    CJNE A, #32, check_line2
-    ; Jesli R5 == 32 (ekran pelny)
-    MOV R5, #0              ; Zerujemy licznik
-    LCDcntrlWR #CLEAR       ; Czyszczenie ekranu (i kursor wraca do 1. linii)
-    SJMP print_char         
+    XRL A, #MAX_CHARS
+    JNZ check_line2
+    MOV R5, #0
+    LCDcntrlWR #CLEAR
+    SJMP print_char
 
 check_line2:
-    CJNE A, #16, print_char
-    ; Jesli R5 == 16 (pierwsza linia pelna)
-    LCDcntrlWR #HOM2        ; Przenies kursor na start drugiej linii
+    MOV A, R5
+    XRL A, #LINE2_POS
+    JNZ print_char
+    LCDcntrlWR #HOM2
 
 print_char:
-    MOV DPTR, #ASCII_CHARS  ; Adres tabeli z odpowiednikami ASCII
-    MOV A, R2               ; Ten sam indeks, ktory znalezlismy wczesniej
-    MOVC A, @A+DPTR         ; Pobranie kodu ASCII do akumulatora
-    ACALL putcharLCD        ; Wyswietlenie znaku
-    INC R5                  ; Zwiekszenie licznika znakow na ekranie
-    SJMP scan_matrix        ; Gotowe! Wroc do czekania na kolejny klawisz
-
-; --- TABELE DANYCH (ROM) ---
+    MOV DPTR, #ASCII_CHARS
+    MOV A, R2
+    MOVC A, @A+DPTR
+    ACALL putcharLCD
+    INC R5
+    LJMP scan_matrix
+	
+; --- TABELE ---
 
 SCAN_CODES:
-    ; Kody z dokumentacji: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, *, #
     DB 0EBH, 077H, 07BH, 07DH, 0B7H, 0BBH, 0BDH, 0D7H, 0DBH, 0DDH
     DB 07EH, 0BEH, 0DEH, 0EEH, 0E7H, 0EDH, 00H
 
 ASCII_CHARS:
-    ; Odpowiadajace im kody ASCII, ktore trafia na LCD
     DB '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
     DB 'A', 'B', 'C', 'D', '*', '#'
 
-    NOP
-    NOP
-    NOP
-end_prog: 
+end_prog:
     SJMP $
 
     END
--- lab4/zad3.asm
+++ lab4/zad3.asm
@@ -1,24 +1,20 @@
 $NOMOD51
 $INCLUDE (reg517.inc)
 
-; --- DEFINICJE PORTÓW I ZMIENNYCH ---
-P5 equ 0F8H
-P7 equ 0DBH
+; P5 equ 0F8H  <-- USUNIĘTE (A10)
+; P7 equ 0DBH  <-- USUNIĘTE (A10)
     
 LCDstatus  equ 0FF2EH       
 LCDcontrol equ 0FF2CH       
 LCDdataWR  equ 0FF2DH       
-
 #define  INITDISP 0x38     
 #define  LCDON    0x0E     
 #define  CLEAR    0x01     
 
     ORG 0000H
     LJMP start
-
     ORG 0100H
-        
-; --- MAKRA BEZPIECZNE ---
+
 LCDcntrlWR MACRO x
       LOCAL loop
       PUSH DPH              
@@ -56,7 +52,6 @@ init_LCD MACRO
       LCDcntrlWR #LCDON
       ENDM
 
-; --- PROCEDURA OPÓŹNIAJĄCA (DEBOUNCING) ---
 delay:  
         MOV R0, #15H
 one:    MOV R1, #0FFH
@@ -70,93 +65,77 @@ putcharLCD:
         LCDcharWR
         RET
 
-; --- PROGRAM GŁÓWNY ---
-
 start:  
     init_LCD
 
 scan_matrix:
-    ; --- ETAP 1: Czekanie na wciśniecie (Skanowanie) ---
-    MOV P5, #0EFH           ; Linia 4
+    MOV P5, #0EFH
     MOV A, P7
     ANL A, #0FH
     CJNE A, #0FH, key_pressed
-
-    MOV P5, #0DFH           ; Linia 3
+    MOV P5, #0DFH
     MOV A, P7
     ANL A, #0FH
     CJNE A, #0FH, key_pressed
-
-    MOV P5, #0BFH           ; Linia 2
+    MOV P5, #0BFH
     MOV A, P7
     ANL A, #0FH
     CJNE A, #0FH, key_pressed
-
-    MOV P5, #07FH           ; Linia 1
+    MOV P5, #07FH
     MOV A, P7
     ANL A, #0FH
     CJNE A, #0FH, key_pressed
-
-    SJMP scan_matrix        ; Powtarzaj, dopóki nic nie wciśnięto
+    SJMP scan_matrix
 
 key_pressed:
-    ; Rekonstrukcja kodu z P5 i P7
-    MOV B, A                ; Chwilowo przechowaj stan kolumn w B
-    MOV A, P5               ; Pobierz stan wierszy
-    ANL A, #0F0H            ; Zostaw tylko najstarsze bity wierszy
-    ORL A, B                ; Połącz wiersze (starsze 4 bity) i kolumny (młodsze 4 bity)
-    
-    ; --- ETAP 2: ZAMROŻENIE KODU SKANINGOWEGO ---
-    MOV R4, A               ; Zapisz zrekonstruowany kod do R4, aby go nie stracić
+    MOV B, A
+    MOV A, P5
+    ANL A, #0F0H
+    ORL A, B
+    MOV R4, A
 
-    ; --- ETAP 3: BLOKADA REPETYCJI (CZEKANIE NA PUSZCZENIE) ---
 wait_for_release:
-    MOV P5, #00H            ; Podaj zera na WSZYSTKIE wiersze klawiatury jednocześnie
-    MOV A, P7               ; Odczytaj stan kolumn
-    ANL A, #0FH             ; Interesują nas tylko 4 najmłodsze bity (P7.0 - P7.3)
-    
-    ; Jeśli puszczono WSZYSTKIE klawisze, stan P7 wyniesie 0FH (1111b z powodu rezystorów podciągających)
-    CJNE A, #0FH, wait_for_release 
+    MOV P5, #00H
+    MOV A, P7
+    ANL A, #0FH
+    CJNE A, #0FH, wait_for_release
+    ACALL delay
 
-    ; --- ETAP 4: DEBOUNCING (TŁUMIENIE DRGAŃ) ---
-    ACALL delay             ; Odczekaj chwilę, aby uniknąć wykrycia zakłóceń mechanicznych styku
-
-    ; --- ETAP 5: WŁAŚCIWE WYKONANIE OPERACJI ---
-    ; Dopiero tutaj przystępujemy do odkodowania i wyświetlenia znaku!
-    MOV A, R4               ; Odzyskaj zamrożony kod z R4
-    MOV R2, #0              ; R2 to nasz indeks do przeszukiwania tabeli
-    MOV DPTR, #SCAN_CODES   
+    MOV R2, #0
+    MOV DPTR, #SCAN_CODES
+    MOV A, R4              ; <-- załaduj R4 do A RAZ przed pętlą
 
 find_loop:
+    PUSH ACC               ; zachowaj R4 (w A)
     MOV A, R2
-    MOVC A, @A+DPTR
-    JZ scan_matrix          ; Zabezpieczenie: jeśli 00H (koniec tabeli), zignoruj i wróć
-    CJNE A, R4, next_code   
-    SJMP print_char         
-
-next_code:
+    MOVC A, @A+DPTR        ; pobierz bajt z tabeli
+    MOV B, A               ; B = bajt z tabeli
+    POP ACC                ; A = R4 (szukany kod)
+    JZ  scan_matrix        ; B==0 → koniec tabeli (sprawdź przed XRL)
+    XRL A, B               ; A = 0 gdy A==B  (fix A22!)
+    JZ  print_char         ; dopasowanie!
+    MOV A, R4              ; przywróć R4 do A na kolejną iterację
     INC R2
-    SJMP find_loop
+    LJMP find_loop         ; LJMP zamiast SJMP (fix A51)
 
 print_char:
     MOV DPTR, #ASCII_CHARS  
     MOV A, R2               
     MOVC A, @A+DPTR         
     ACALL putcharLCD        
-    
-    SJMP scan_matrix        ; Wróć do nasłuchiwania kolejnego znaku
+    SJMP scan_matrix
 
-; --- TABELE DANYCH (ROM) ---
 SCAN_CODES:
-    DB 0EBH, 077H, 07BH, 07DH, 0B7H, 0BBH, 0BDH, 0D7H, 0DBH, 0DDH, 07EH, 0BEH, 0DEH, 0EEH, 0E7H, 0EDH, 00H
+    DB 0EBH, 077H, 07BH, 07DH, 0B7H, 0BBH, 0BDH, 0D7H, 0DBH, 0DDH
+    DB 07EH, 0BEH, 0DEH, 0EEH, 0E7H, 0EDH, 00H
 
 ASCII_CHARS:
-    DB '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', '*', '#'
+    DB '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+    DB 'A', 'B', 'C', 'D', '*', '#'
 
     NOP
     NOP
     NOP
 end_prog: 
     SJMP $
-
     END
